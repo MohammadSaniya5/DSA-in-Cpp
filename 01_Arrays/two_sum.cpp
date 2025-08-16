e7bbdf2208e7354fecf521f41867474c2f5b64b2
Mohammad Saniya <saniyamohammad0567@gmail.com>
2025-08-16 13:03:22 +0530

    Added Two Sum problem (LeetCode #1) in Arrays

--- 01_Arrays/two_sum.cpp
+++ 01_Arrays/two_sum.cpp
@@ -1,14 +1,27 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> mp;
-        for(int i=0;i<nums.size();i++){ 
-            int complement=target-nums[i];
-            if(mp.find(complement)!=mp.end()){
-                return {mp[complement],i};
-            }
-            mp[nums[i]]=i;
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (mp.find(complement) != mp.end()) {
+            return {mp[complement], i};
         }
-        return {};
+        mp[nums[i]] = i;
     }
-};
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = twoSum(nums, target);
+
+    cout << "Indices: ";
+    for (int idx : result) cout << idx << " ";  // Expected Output: 0 1
+    cout << endl;
+
+    return 0;
+}
